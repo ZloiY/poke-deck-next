55a0b8f785a5076a7e56f92f855555a629a30919
Maksim Bakhanovich <maksim.bakhanovich@protonmail.com>
2023-02-18 18:34:32 +0100

    feat(grid): added animation for card changing out of pagination events

--- src/components/CardsGrid.tsx
+++ src/components/CardsGrid.tsx
@@ -1,11 +1,11 @@
 import { Pokemon } from "pokenode-ts";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { a, config, useSpring } from "@react-spring/web";
 
 import { useLoadingState, usePaginationState } from "../hooks";
 import { FlipCard } from "./Cards";
-import { unknown } from "zod";
 
 export const CardsGrid = ({
   pokemons,
@@ -16,6 +16,10 @@ export const CardsGrid = ({
 }) => {
   const loadingState = useLoadingState();
   const paginationState = usePaginationState();
+  const [parent, toggleAnimation] = useAutoAnimate({
+    duration: 500,
+    easing: "ease-out",
+  });
 
   const [{ position, left }, api] = useSpring(() => ({
     from: { position: "absolute", left: 0 },
@@ -26,47 +30,59 @@ export const CardsGrid = ({
 
   useEffect(() => {
     switch (true) {
+      case paginationState == "Initial": {
+        toggleAnimation(true);
+        break;
+      }
       case loadingState == "Started" && paginationState == "Next": {
         api.start({
           from: { position: "absolute", left: 0 },
-          to: [{ position: "absolute", left: -2000 }],
+          to: [{ position: "absolute", left: -5000 }],
+          config: { tension: 120, friction: 100 },
         });
         break;
-      } 
+      }
       case loadingState == "Started" && paginationState == "Prev": {
         api.start({
           from: { position: "absolute", left: 0 },
-          to: [{ position: 'absolute', left: 2000 }]
+          to: [{ position: "absolute", left: 5000 }],
+          config: { tension: 120, friction: 100 },
         });
         break;
       }
       case loadingState == "Finished" && paginationState == "Next": {
         api.start({
-          from: { position: "absolute", left: 2000 },
+          from: { position: "absolute", left: 5000 },
           to: [{ position: "absolute", left: 0 }],
+          config: config.slow,
         });
         break;
       }
       case loadingState == "Finished" && paginationState == "Prev": {
         api.start({
-          from: { position: "absolute", left: -2000 },
-          to: [{ position: "absolute", left: 0 }]
-        })
+          from: { position: "absolute", left: -5000 },
+          to: [{ position: "absolute", left: 0 }],
+          config: config.slow,
+        });
         break;
-      } 
+      }
+      default: {
+        toggleAnimation(false);
+      }
     }
   }, [loadingState, paginationState]);
 
   return (
     <div className="h-full relative">
       <a.div
-        style={{ position: position as unknown as 'static', left }}
+        style={{ position: position as unknown as "static", left }}
+        ref={parent}
         className="w-full grid gap-5 min-[1880px]:grid-cols-6 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2
 mt-5"
       >
         {pokemons?.map((pokemon) => (
           <FlipCard
-            key={pokemon.name}
+            key={pokemon?.name}
             pokemon={pokemon}
             keepFlipped={cardsFlipped}
           />
